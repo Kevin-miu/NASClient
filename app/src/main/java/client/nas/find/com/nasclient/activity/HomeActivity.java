baba9cbd0c09e86cf741f1d22a0c730758a67322
Kevin_miu <791693310@qq.com>
2018-12-12 14:52:22 +0800

    1.修改fragment管理 2.增加sharepreference

--- app/src/main/java/client/nas/find/com/nasclient/activity/HomeActivity.java
+++ app/src/main/java/client/nas/find/com/nasclient/activity/HomeActivity.java
@@ -2,7 +2,10 @@ package client.nas.find.com.nasclient.activity;
 
 import android.Manifest;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import android.view.MenuItem;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.PermissionListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import client.nas.find.com.nasclient.R;
@@ -28,12 +32,19 @@ import client.nas.find.com.nasclient.util.CommomUtil;
 public class HomeActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE_WRITE_EXTERNAL_STORAGE = 100;
+    private static final String CURRENT_FRAGMENT = "STATE_FRAGMENT_SHOW";
 
     CloudFragment cloudFragment;
     PointFragment pointFragment;
     SettingFragment settingFragment;
     FileFragment fileFragment;
-    Fragment currentFragment;
+
+    private Fragment currentFragment;
+    private FragmentManager manager;
+    private String[] fragmentTagList = {"CloudFragment", "PointFragment", "SettingFragment", "FileFragment"};
+    private List<Fragment> fragmentList = new ArrayList<>();
+
+    private int currentIndex = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,18 +54,13 @@ public class HomeActivity extends AppCompatActivity {
 
             switch (item.getItemId()) {
                 case R.id.navigation_cloud:
-                    //Toast.makeText(HomeActivity.this, "点击云盘", Toast.LENGTH_LONG).show();
-                    //禁止返回键退出
-                    //getFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.container_layout, cloudFragment).commit();
-                    getFragmentManager().beginTransaction().replace(R.id.container_layout, cloudFragment).commit();
+                    switchFragment(fragmentList.get(0), fragmentTagList[0]);
                     return true;
                 case R.id.navigation_point:
-                    //Toast.makeText(HomeActivity.this, "点击积分", Toast.LENGTH_LONG).show();
-                    getFragmentManager().beginTransaction().replace(R.id.container_layout, pointFragment).commit();
+                    switchFragment(fragmentList.get(1), fragmentTagList[1]);
                     return true;
                 case R.id.navigation_setting:
-                    //Toast.makeText(HomeActivity.this, "点击设置", Toast.LENGTH_LONG).show();
-                    getFragmentManager().beginTransaction().replace(R.id.container_layout, settingFragment).commit();
+                    switchFragment(fragmentList.get(2), fragmentTagList[2]);
                     return true;
             }
             return false;
@@ -74,8 +80,23 @@ public class HomeActivity extends AppCompatActivity {
         AndPermission.with(this).requestCode(PERMISSION_CODE_WRITE_EXTERNAL_STORAGE)
                 .permission(Manifest.permission.WRITE_EXTERNAL_STORAGE).send();
 
-        //初始化控件
-        initView();
+        // 下方菜单栏监听事件，已经重写
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //获取manager
+        manager = getFragmentManager();
+
+        //内存重启时调用
+        if (savedInstanceState != null) {
+
+            //获取到“内存重启”时保留下来的索引
+            currentIndex = savedInstanceState.getInt(CURRENT_FRAGMENT, 0);
+            reView(currentIndex);
+
+        } else {
+            initView();
+        }
 
     }
 
@@ -85,27 +106,120 @@ public class HomeActivity extends AppCompatActivity {
      */
     private void initView() {
 
-        cloudFragment = new CloudFragment();
-        pointFragment = new PointFragment();
-        settingFragment = new SettingFragment();
-
-        //设置Context
-        cloudFragment.setContext(HomeActivity.this);
-        pointFragment.setContext(HomeActivity.this);
-        settingFragment.setContext(HomeActivity.this);
-
-        fileFragment = new FileFragment();
-        fileFragment.setContext(HomeActivity.this);
-
         // 下方菜单栏监听事件，已经重写
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        //设置初始Fragment
-        getFragmentManager().beginTransaction().replace(R.id.container_layout, cloudFragment).commit();
+        cloudFragment = new CloudFragment();
+        pointFragment = new PointFragment();
+        settingFragment = new SettingFragment();
+        fileFragment = null;
+        //fileFragment = new FileFragment();
+
+        cloudFragment.setContext(HomeActivity.this);
+        pointFragment.setContext(HomeActivity.this);
+        settingFragment.setContext(HomeActivity.this);
+        //fileFragment.setContext(HomeActivity.this);
+
+        cloudFragment.setOnSwitchFragment(new CloudFragment.OnSwitchFragment() {
+            @Override
+            public void switchFragmentToFileFragment(Fragment fragment) {
+                //更新fragmentList
+                fileFragment = (FileFragment) fragment;
+
+                fileFragment.setOnSwitchFragment(new FileFragment.OnSwitchFragment() {
+                    @Override
+                    public void switchFragmentTocCloudFragment() {
+                        switchFragment(fragmentList.get(0), fragmentTagList[0]);
+                    }
+                });
+
+                fragmentList.add(3, fileFragment);
+
+                switchFragment(fragmentList.get(3), fragmentTagList[3]);
+            }
+        });
+
+        fragmentList.add(0, cloudFragment);
+        fragmentList.add(1, pointFragment);
+        fragmentList.add(2, settingFragment);
+        //fragmentList.add(3, fileFragment);
+
+        //获取当前（默认）的fragment
+        currentFragment = fragmentList.get(0);
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container_layout, currentFragment, fragmentTagList[0]);
+        transaction.commitAllowingStateLoss();
 
     }
 
+
+    /**
+     * 当内存重启的时候，调用该方法
+     *
+     * @param index
+     */
+    private void reView(int index) {
+
+        fragmentList.removeAll(fragmentList);
+
+        fragmentList.add(manager.findFragmentByTag(fragmentTagList[0]));
+        fragmentList.add(manager.findFragmentByTag(fragmentTagList[1]));
+        fragmentList.add(manager.findFragmentByTag(fragmentTagList[2]));
+        fragmentList.add(manager.findFragmentByTag(fragmentTagList[3]));
+
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        for (int i = 0; i < fragmentList.size(); i++) {
+            if (i == index) {
+                transaction.show(fragmentList.get(i));
+            } else {
+                transaction.hide(fragmentList.get(i));
+            }
+        }
+
+        transaction.commitAllowingStateLoss();
+        currentFragment = fragmentList.get(index);
+
+    }
+
+    /**
+     * 切换fragment
+     *
+     * @param to
+     * @param tag
+     */
+    private void switchFragment(Fragment to, String tag) {
+        if (currentFragment != to) {
+            FragmentTransaction transaction = manager.beginTransaction();
+
+            if (!(currentFragment instanceof FileFragment)) {
+                if (!to.isAdded() ) {
+                    //没有被添加
+                    //隐藏当前的，添加新的，显示新的
+                    transaction.hide(currentFragment).add(R.id.container_layout, to, tag).show(to);
+                } else {
+                    //已经添加
+                    //隐藏当前的，显示新的
+                    transaction.hide(currentFragment).show(to);
+                }
+            } else {
+                //如果当前fragment是fileFragment的话就移除
+                if (!to.isAdded() ) {
+                    //没有被添加
+                    //隐藏当前的，添加新的，显示新的
+                    transaction.remove(currentFragment).add(R.id.container_layout, to, tag).show(to);
+                } else {
+                    //已经添加
+                    //隐藏当前的，显示新的
+                    transaction.remove(currentFragment).show(to);
+                }
+            }
+            currentFragment = to;
+            transaction.commitAllowingStateLoss();
+        }
+    }
 
     /**
      * 手机按键的监听事件
@@ -121,8 +235,6 @@ public class HomeActivity extends AppCompatActivity {
             //按下返回键
             if (keyCode == KeyEvent.KEYCODE_BACK) {
                 Log.e("Tag", "点击了返回键");
-                //确认当前处于哪个Fragment
-                currentFragment = getFragmentManager().findFragmentById(R.id.container_layout);
 
                 if (currentFragment instanceof CloudFragment) {
                     cloudFragment.exit();
@@ -146,6 +258,15 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        //activity在销毁时让fragment也跟着销毁
+        outState.putInt(CURRENT_FRAGMENT, currentIndex);
+        //super.onSaveInstanceState(outState, outPersistentState);
+
     }
 
     @Override
